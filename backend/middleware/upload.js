3caa1ec2ad098f36d9f0d0543ba0721482c6dad0
Prahlad Singh <prahladsingh7512@gmail.com>
2025-08-20 17:14:56 +0530

    Update upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -25,5 +25,5 @@ const storage = new CloudinaryStorage({
 
 export const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  limits: { fileSize: 100 * 1024 * 1024 }, // 5MB limit
 });
